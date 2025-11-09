483f492c2279c2d58e9a21a31b7358013dbcd097
Mohamed Abdulla <abdabdulla78@gmail.com>
2025-11-09 23:01:36 +0530

    feat: enhance user page with post data display and improve global styles

--- src/app/(root)/users/[id]/page.tsx
+++ src/app/(root)/users/[id]/page.tsx
@@ -10,9 +10,20 @@ interface PageProps {
 const Page: FC<PageProps> = async ({ params }) => {
   const { id } = await params;
 
+  ///get post from json placeholder
+  const post = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const postData = await post.json();
+
   //match the id with user data
   const user = USERS.find((user) => user.id === Number(id));
-  return <div>{user ? user.name : "User not found"}</div>;
+
+  return (
+    <div className="space-y-4">
+      <h1 className="text-red-500">{user ? user.name : "User not found"}</h1>
+      <h2 className="text-blue-500">Post Title: {postData.title}</h2>
+      <p className="text-gray-700">{postData.body}</p>
+    </div>
+  );
 };
 
 export default Page;
